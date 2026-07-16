c14a843d22c78de2ae25f1a7b7e3a76ced13bff4
AdamMusa <adammusa2222@gmail.com>
2026-07-16 05:32:40 +0000

    deploying ~5bdd98db

--- .nixpacks/build.sh
+++ .nixpacks/build.sh
@@ -1,1 +1,1 @@
-docker build . -f ./.nixpacks/Dockerfile -t 9510fe1c-e09a-47e1-8fc9-8960d2b0f793 --build-arg NIXPACKS_METADATA=dart
+docker build . -f ./.nixpacks/Dockerfile -t 7501a604-15bc-48e8-9e5a-3fb98a63c475 --build-arg NIXPACKS_METADATA=dart
